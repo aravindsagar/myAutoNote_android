48ece4e20003c3297a9846e1cc5794d09e343463
TriCoder126 <dman126@yahoo.com>
2017-11-16 16:11:36 -0600

    json file

--- MyApplication/app/src/main/java/paradigm/shift/myautonote/MyNotes.java
+++ MyApplication/app/src/main/java/paradigm/shift/myautonote/MyNotes.java
@@ -37,7 +37,7 @@ public class MyNotes extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-
+    //Drews comment
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
