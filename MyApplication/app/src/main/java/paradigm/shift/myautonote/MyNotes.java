884db9fa76f701c29fdf7d3eec2954c7a12ccb15
Emily Chen <echen35@illinois.edu>
2017-11-20 17:49:11 -0600

    clickable search button

--- MyApplication/app/src/main/java/paradigm/shift/myautonote/MyNotes.java
+++ MyApplication/app/src/main/java/paradigm/shift/myautonote/MyNotes.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import paradigm.shift.myautonote.adapter.DirListAdapter;
 import paradigm.shift.myautonote.data_util.DataReader;
@@ -76,6 +77,24 @@ public class MyNotes extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.my_notes, menu);
+
+        //Link MyNotes to Search button
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        MenuItem item = menu.findItem(R.id.menuSearch);
+        SearchView searchView = (SearchView)item.getActionView();
+
+        searchView.setOnQueryTextListener( new SearchView.OnQueryTextListener(){
+            @Override
+            public boolean onQueryTextSubmit(String query){
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText){
+                return false;
+            }
+        });
         return true;
     }
 
@@ -124,4 +143,6 @@ public class MyNotes extends AppCompatActivity
         myDirListAdapter.itemClick(position);
 
     }
+
+
 }
