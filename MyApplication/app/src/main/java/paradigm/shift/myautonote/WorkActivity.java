0dcbacc409fadc3d625941dd25338eddb75f39e9
TriCoder126 <dman126@yahoo.com>
2017-11-23 15:51:47 -0600

    added missing files

---
+++ MyApplication/app/src/main/java/paradigm/shift/myautonote/WorkActivity.java
@@ -0,0 +1,276 @@
+package paradigm.shift.myautonote;
+
+import android.graphics.Typeface;
+import android.os.Handler;
+import android.support.design.widget.NavigationView;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.Html;
+import android.text.Layout;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.Scroller;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import paradigm.shift.myautonote.data_model.LineObject;
+
+import static android.support.v4.view.ViewCompat.setOverScrollMode;
+import static android.view.View.OVER_SCROLL_IF_CONTENT_SCROLLS;
+
+public class WorkActivity extends AppCompatActivity{
+
+    private String[] content;
+    private ArrayList<LineObject> lineData;
+
+    private int pad1 = 0;
+    private int pad2 = 0;
+    private int pad3 = 0;
+
+    private ScrollView scrollView;
+    private LinearLayout formattedViewer;
+    private TextView currentLine;
+    private int workingIndex = 0;
+    private EditText editor;
+    private int editorOffset = 0;
+    private boolean switchLine = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_work);
+
+        //TODO- does not work
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        setTitle(getIntent().getStringExtra("note_title"));
+
+        formattedViewer = (LinearLayout) findViewById(R.id.formatted_viewer);
+        scrollView = (ScrollView) findViewById(R.id.scrollable_viewer);
+
+
+        String givenData = getIntent().getStringExtra("note_content");
+        if(givenData.length() > 0){
+            content = givenData.split("<p>");
+            lineData = new ArrayList<LineObject>();
+            for(int i = 1; i < content.length; i++){
+
+                TextView newView = createNewTextView(i-1);
+
+                String parsableText = "";
+                if(content[i].length() > 4 && content[i].substring(content[i].length()-4).equals("</p>"))
+                    parsableText = content[i].substring(0, content[i].length()-4);
+                else
+                    parsableText = content[i];
+
+                LineObject lo = new LineObject(i-1, parsableText, pad1, pad2, pad3, false);
+                lo.printLineObject(this, newView);
+                setPadding(lo);
+                lineData.add(lo);
+                formattedViewer.addView(newView);
+
+            }
+        }
+
+        workingIndex = lineData.size();
+
+        currentLine = createNewTextView(workingIndex);
+
+
+        LineObject lo = new LineObject(workingIndex, "", pad1, pad2, pad3, true);
+        lo.printLineObject(this, currentLine);
+        setPadding(lo);
+        lineData.add(lo);
+        formattedViewer.addView(currentLine);
+
+        editor = (EditText) findViewById(R.id.edit_box);
+        editor.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if((currentLine.getY()+currentLine.getHeight()) > (scrollView.getScrollY()+scrollView.getHeight()) || currentLine.getY() < scrollView.getScrollY()){
+                            if((int)currentLine.getY() > 0)
+                                scrollView.smoothScrollTo(0, (int)currentLine.getY()-scrollView.getHeight()/2);
+                        }
+                    }
+                }, 100);
+            }
+        });
+
+
+        editor.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {}
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {}
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                if(!switchLine){
+                    if(s.length() > 0 && s.toString().indexOf("\n") == s.length()-1){
+                        switchLine = true;
+
+                        lineData.get(workingIndex).endWork(currentLine);
+
+                        //TODO split line if in middle
+                        editor.setText("");
+
+
+                        for(int i = lineData.size()-1; i > workingIndex; i--){// insert id in between
+                            findViewById(i).setId(i+1);
+                        }
+                        workingIndex++;
+                        currentLine = createNewTextView(workingIndex);
+                        LineObject lo = new LineObject(workingIndex, "", pad1, pad2, pad3, true);
+                        lo.printLineObject(WorkActivity.this, currentLine);
+                        setPadding(lo);
+                        if(workingIndex >= lineData.size()){
+                            lineData.add(lo);
+                            formattedViewer.addView(currentLine);
+                        }else{
+                            lineData.add(workingIndex, lo);
+                            formattedViewer.addView(currentLine, workingIndex);
+                        }
+
+                        switchLine = false;
+                    }else{
+                        currentLine.setText(editor.getText().toString());
+                        lineData.get(workingIndex).content = editor.getText().toString();
+
+                        formatLines(workingIndex);
+
+                    }
+                    final Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            if((currentLine.getY()+currentLine.getHeight()) > (scrollView.getScrollY()+scrollView.getHeight()) || currentLine.getY() < scrollView.getScrollY()){
+                                if((int)currentLine.getY() > 0)
+                                    scrollView.smoothScrollTo(0, (int)currentLine.getY()-scrollView.getHeight()/2);
+                            }
+                        }
+                    }, 150);
+                }
+            }
+        });
+    }
+
+    public View.OnClickListener onClickLineListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(final View v) {
+            TextView lineView = (TextView)v;
+
+            if(lineView.getId() != currentLine.getId()){
+                switchLine = true;
+                lineData.get(workingIndex).endWork(currentLine);
+                currentLine = lineView;
+                workingIndex = (int)lineView.getId();
+                editor.setText(lineView.getText());
+                if(workingIndex == 0)
+                    lineData.get(workingIndex).copyPaddingFromPreviousLine(null);
+                else
+                    lineData.get(workingIndex).copyPaddingFromPreviousLine(lineData.get(workingIndex-1));
+
+                lineData.get(workingIndex).content = currentLine.getText().toString();
+                setPadding(lineData.get(workingIndex));
+                currentLine.setOnClickListener(onClickLineListener);
+                currentLine.setBackgroundColor(ContextCompat.getColor(WorkActivity.this, R.color.textHighlight));
+                editor.requestFocus();
+                switchLine = false;
+            }
+
+            InputMethodManager keyboard = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+            keyboard.showSoftInput(editor, 0);
+            final Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    editor.setSelection(editorOffset);
+                    if((currentLine.getY()+currentLine.getHeight()) > (scrollView.getScrollY()+scrollView.getHeight()) || currentLine.getY() < scrollView.getScrollY()){
+                        if((int)currentLine.getY() > 0)
+                            scrollView.smoothScrollTo(0, (int)currentLine.getY()-scrollView.getHeight()/2);
+                    }
+                }
+            }, 150);
+
+        }
+    };
+
+    public View.OnTouchListener onTouchLineListener = new View.OnTouchListener() {
+
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+
+            if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                Layout layout = ((TextView) view).getLayout();
+
+                    int x = (int)motionEvent.getX();
+                    int y = (int)motionEvent.getY();
+                    editorOffset =  0;
+                    if (layout!=null){
+                        int line = layout.getLineForVertical(y);
+                        editorOffset = layout.getOffsetForHorizontal(line, x-view.getPaddingLeft());
+                    }
+            }
+            return false;
+        }
+    };
+
+    private void formatLines(int startIndex){
+        for(int i = startIndex; i < lineData.size(); i++){
+
+            TextView newView = findViewById(i);
+            LineObject lo = lineData.get(i);
+            lo.printLineObject(this, newView);
+            setPadding(lo);
+        }
+
+    }
+
+    private TextView createNewTextView(int index){
+        TextView temp = new TextView(this);
+        temp.setId(index);
+        temp.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        temp.setOnClickListener(onClickLineListener);
+        temp.setOnTouchListener(onTouchLineListener);
+        return temp;
+    }
+
+
+    private void setPadding(LineObject lo){
+        pad1 = lo.pad1;
+        pad2 = lo.pad2;
+        pad3 = lo.pad3;
+    }
+
+
+
+}
---
+++ MyApplication/app/src/main/java/paradigm/shift/myautonote/data_model/LineObject.java
@@ -0,0 +1,221 @@
+package paradigm.shift.myautonote.data_model;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v4.content.ContextCompat;
+import android.text.Html;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import paradigm.shift.myautonote.R;
+
+/**
+ * Created by Drew on 11/20/2017.
+ */
+
+public class LineObject {
+
+    public int index;
+    public String content;
+    public int headerSize = 0;
+    public int vocab = -1;
+
+    public int pad1 = 0;
+    public int pad2 = 0;
+    public int pad3 = 0;
+
+    public boolean working = false;
+
+    public LineObject(int idx, String c, int p1, int p2, int p3, Boolean w){
+        index = idx;
+        content = c;
+
+
+        pad1 = p1;
+        pad2 = p2;
+        pad3 = p3;
+
+        working = w;
+
+
+
+
+
+    }
+
+    public void copyPaddingFromPreviousLine(LineObject other){
+        if(other == null){
+            pad1 = 0;
+            pad2 = 0;
+            pad3 = 0;
+        }else{
+            pad1 = other.pad1;
+            pad2 = other.pad2;
+            pad3 = other.pad3;
+        }
+
+    }
+
+    public void setPadding(int p1, int p2, int p3){
+        pad1 = p1;
+        pad2 = p2;
+        pad3 = p3;
+    }
+
+    public void endWork(TextView view){
+        view.setBackgroundColor(0x00000000);
+        working = false;
+    }
+
+
+
+    public void printLineObject(Context context, TextView output){
+        headerSize = 0;
+        vocab = -1;
+
+        isHeader();
+        isTitle();
+
+
+        //valueTV.setTextColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        //valueTV.setTextSize(16);
+        //valueTV.setPadding(50,0,0,0);
+        //valueTV.setTypeface(Typeface.DEFAULT_BOLD);
+
+        int [] fontSizeTypes = { 14, 22, 20, 18, 17, 17, 17 };
+
+
+
+        int color = R.color.textColor;
+        double opacity = 1;
+
+        if(content.trim().length() == 0){
+            if(working){
+                output.setText("new line");
+                output.setTypeface(null, Typeface.ITALIC);
+            }
+        }else {
+
+            if (headerSize > 0) {
+                color = R.color.textHeaderColor;
+                opacity = 1 - (0.1) * headerSize;
+
+                if (headerSize == 4) {
+                    //this.paddingLevel == [];
+                    //data.padding = "";
+                    pad3 = 5;
+                    pad1 = 0;
+                    pad2 = 0;
+                }
+                if (headerSize == 3) {
+                    pad2 = 3;
+                    pad1 = 0;
+                }
+                if (headerSize == 2) {
+                    pad1 = 2;
+                }
+
+                output.setTypeface(null, Typeface.BOLD);
+                output.setAlpha((float)opacity);
+            }else{
+                output.setTypeface(null, Typeface.NORMAL);
+            }
+
+        }
+
+        if(working){
+            output.setBackgroundColor(ContextCompat.getColor(context, R.color.textHighlight));
+        }
+        output.setTextColor(ContextCompat.getColor(context, color));
+        output.setTextSize(fontSizeTypes[headerSize]);
+        int paddingLeft = 0;
+        int paddingTop = 0;
+        if(headerSize > 0){
+            if(headerSize != 4)
+                paddingLeft += pad3;
+            if(headerSize != 3)
+                paddingLeft += pad2;
+            if(headerSize != 2)
+                paddingLeft += pad1;
+            paddingTop = 50;
+        }else
+            paddingLeft += pad1+pad2+pad3;
+
+        output.setPadding(paddingLeft*15,paddingTop, 0, 0);
+        output.setText(Html.fromHtml(content));
+
+        if(headerSize == 1)
+            pad3 = 5;
+
+    }
+
+    private void isHeader(){
+        int hs = 0;
+        headerSize = 0;
+        int i = content.length()-1;
+        while(i >= 0 && content.charAt(i) == '`'){
+            hs++;
+            i--;
+        }
+        content = content.substring(0, content.length()-hs);
+        if(hs > 6)
+            hs =  6;
+        headerSize = hs;
+        Log.v("header1", ""+headerSize);
+    }
+
+
+    private void isTitle(){
+        //console.log(nextContent);
+        String[] words;
+        int score = 0;
+
+        if(headerSize == 0 && vocab == -1){
+
+            words = content.split(" ");
+            score = 0;
+            for(int i = 0; i < words.length; i++){
+                if(words[i].length() > 0)
+                    score = (this.isWordUperCase(words[i])|| this.containsIntegers(words[i]) ? score+1 : score);
+            }
+            if((double)score/words.length >= 0.6){
+                //if(nextIsHeader)
+                //data.headerSize = 3;
+                //  else
+                if(pad3 > 0){
+                    if(pad2 > 0)
+                        headerSize = 2;
+                    else
+                        headerSize = 2;
+
+                }else
+                    headerSize = 3;
+
+
+
+                //data.headerSize = 2;
+
+            }
+
+        }
+        Log.v("header", ""+headerSize);
+
+    }
+
+    private boolean isWordUperCase(String input){
+        if(input.charAt(0) >= 65 && input.charAt(0) <=  90)
+            return true;
+        else
+            return false;
+    }
+
+    private boolean containsIntegers(String input){
+        if(input.charAt(0) >= 48 && input.charAt(0) <=  57)
+            return true;
+        else
+            return false;
+    }
+}
+
